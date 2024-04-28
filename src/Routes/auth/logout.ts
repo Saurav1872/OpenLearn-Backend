eefc6f365533616bea6835f68a4d761eb6821d98
Saurav1872 <saurav.suman1807@gmail.com>
2024-04-28 18:16:13 +0530

    Added removed cookies login in logout

--- src/Routes/auth/logout.ts
+++ src/Routes/auth/logout.ts
@@ -4,7 +4,8 @@ import jwt from 'jsonwebtoken';
 export default function logoutHandler(req:Request,res:Response){
     const token = jwt.sign({},"123",{expiresIn:0});
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.cookie('user',token,{httpOnly:true,maxAge:1});
+    // res.cookie('user',token,{httpOnly:true,maxAge:1});
+    res.clearCookie('user', { httpOnly: true });
     res.status(200).send({
         ok:true,
         message:'successfuly loged out!'
